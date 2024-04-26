0bc27d26ff6f7a82ad9a59d212b0a2dfe5eec67c
Chonthicha Udomsup <nan@sennalabs.com>
2024-04-26 10:52:36 +0700

    fix: implemented ts

--- src/features/hook/pages/UseReducerExample/index.tsx
+++ src/features/hook/pages/UseReducerExample/index.tsx
@@ -1,31 +1,40 @@
 import { useReducer } from "react";
 
-export const reducer = (state, action) => {
+export const reducer = (state: { count: number }, action: { type: string }) => {
   switch (action.type) {
     case "INCREMENT":
-      return { count: state.count + 1, showText: state.showText };
-    case "toggleShowText":
-      return { count: state.count, showText: !state.showText };
+      return { count: state.count + 1 };
+    case "DECREMENT":
+      return { count: state.count - 1 };
     default:
       return state;
   }
 };
 
 export const UseReducerExample = () => {
-  const [state, dispatch] = useReducer(reducer, { count: 0, showText: true });
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
 
   return (
     <div className="p-[40px] text-secondary flex flex-col items-center gap-y-[60px] justify-center">
       <h1>{state.count}</h1>
-      <button
-        onClick={() => {
-          dispatch({ type: "INCREMENT" });
-          dispatch({ type: "toggleShowText" });
-        }}
-      >
-        Show Text
-      </button>
-      {state.showText && <p>Show Text</p>}
+      <div className="flex gap-x-[10px]">
+        <button
+          className="bg-secondary px-[16px] py-[8px] rounded-[8px] text-black w-[150px]"
+          onClick={() => {
+            dispatch({ type: "INCREMENT" });
+          }}
+        >
+          Add
+        </button>
+        <button
+          className="bg-secondary px-[16px] py-[8px] rounded-[8px] text-black w-[150px]"
+          onClick={() => {
+            dispatch({ type: "DECREMENT" });
+          }}
+        >
+          Minus
+        </button>
+      </div>
     </div>
   );
 };
